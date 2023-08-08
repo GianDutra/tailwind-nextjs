60990d644bb24e3c735d42eb21e3b4917c13abc3
GianDutra <chikegang@gmail.com>
2023-08-08 19:26:22 -0300

    Subindo novamente em outro repositório por erros anteriores

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+import { LogOut } from "lucide-react";
+
+export function Profile() {
+  return (
+    <div className="grid items-center gap-3 grid-cols-profile">
+      <img
+        src="https://github.com/GianDutra.png"
+        className="w-10 h-10 rounded-full"
+      />
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Gian Dutra
+        </span>
+        <span className="text-sm text-zinc-500 truncate">
+          giandutra@hotmail.com.br
+        </span>
+      </div>
+      <button type="button" className="ml-auto p-2 hover:bg-zinc-50 rounded-md">
+        <LogOut className="w-5 h-6 text-zinc-500" />
+      </button>
+    </div>
+  );
+}
