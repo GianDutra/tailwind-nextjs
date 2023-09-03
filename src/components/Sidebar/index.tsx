6ef5dcc898551d1379f4b8ff2376078d02e39dcf
GianDutra <chikegang@gmail.com>
2023-09-03 18:50:41 -0300

    add: backup

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,10 +5,11 @@ import { NavItem } from "./MainNavigation/NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
 import { InputControl, InputPrefix, InputRoot } from "../Input";
+import * as Collapsible from '@radix-ui/react-collapsible'
 
 export function Sidebar() {
   return (
-    <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6">
+    <Collapsible.Root className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
       <Logo />
 
       <InputRoot>
@@ -33,6 +34,6 @@ export function Sidebar() {
 
         <Profile />
       </div>
-    </aside>
+    </Collapsible.Root>
   );
 }
