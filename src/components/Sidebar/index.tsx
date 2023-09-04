7a41e2b4ac4d4a76e704198414f09368c86116cc
GianDutra <chikegang@gmail.com>
2023-09-03 21:09:42 -0300

    add: switch light/dark theme button

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -9,12 +9,16 @@ import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { NavItem } from './Navigation/NavItem'
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { Button } from '../Button'
+import { Switch } from './Switch'
 
 export function Sidebar() {
   return (
     <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 overflow-hidden border-b border-zinc-200 bg-white p-4 scrollbar-thin scrollbar-track-zinc-100 scrollbar-thumb-zinc-300 data-[state=open]:bottom-0 dark:border-zinc-800 dark:bg-zinc-900 lg:bottom-0 lg:right-auto lg:h-auto lg:w-80 lg:overflow-auto lg:border-b-0 lg:border-r lg:px-5 lg:py-8">
       <div className="flex items-center justify-between">
-        <Logo />
+        <div className="flex items-center">
+          <Switch />
+          <Logo />
+        </div>
         <Collapsible.Trigger asChild className="lg:hidden">
           <Button variant="ghost">
             <Menu className="h-6 w-6 text-zinc-500" />
